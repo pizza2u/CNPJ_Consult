1887cfce6aad4114b21325a54ce86b08ce2be166
Sara <55607380+pizza2u@users.noreply.github.com>
2024-07-18 23:17:53 -0300

    Add files via upload

---
+++ script.py
@@ -0,0 +1,84 @@
+import time
+import requests
+import csv
+from tqdm import tqdm
+
+def data(cnpj):
+    url = f"https://publica.cnpj.ws/cnpj/{cnpj}" # API pública
+    try:
+        response = requests.get(url)
+        response.raise_for_status() #  se bem sucedida retorna 200
+        dados = response.json() # retorna pela em JSON.
+       # print(dados)  verificar o JSON
+        return dados
+    except requests.RequestException as e:
+        print(f"Erro ao obter dados: {e}") #caso de falha ou limites atingidos
+        return None
+
+def type(natureza_juridica): #tipo de natureza (Municipal, Estadual, Federal)
+    if natureza_juridica:
+        descricao = natureza_juridica.lower()
+        if 'municipal' in descricao:
+            return 'Municipal'
+        elif 'estadual' in descricao:
+            return 'Estadual'
+        elif 'federal' in descricao:
+            return 'Federal'
+    return 'Não Especificado'  #se não for nenhum dos tipos
+
+def save(cnpj, dados, arquivo):
+    try:
+        with open(arquivo, 'a', newline='', encoding='utf-8') as file:
+            writer = csv.writer(file)
+            if file.tell() == 0:  # arquivo vazio para cabeçalho
+                writer.writerow([
+                    'CNPJ', 'Razão Social', 'Porte', 
+                    'Natureza Jurídica', 'Natureza Tipo', 'Tipo(Matriz/Filial)', 
+                    'Atividade Principal', 'Município', 'UF'
+                ])
+            
+            est = dados.get('estabelecimento', {})
+            cidade = est.get('cidade', {})
+            estado = est.get('estado', {})
+            
+            writer.writerow([
+                cnpj,  # CNPJ digitado pelo usuário
+                dados.get('razao_social', ''),
+                dados.get('porte', {}).get('descricao', ''), 
+                dados.get('natureza_juridica', {}).get('descricao', ''), # natureza juridica
+                type(dados.get('natureza_juridica', {}).get('descricao', '')),  #  tipo de natureza
+                est.get('tipo', ''),  # tipo (Filial ou Matriz)
+                est.get('atividade_principal', {}).get('descricao', ''),
+                cidade.get('nome', '') if cidade else '',  # Verifica se cidade não é None e adc o nome
+                estado.get('sigla', '') if estado else '',  # Verifica se estado não é None e adc a UF
+            ])
+    except AttributeError as e:
+        print(f"Erro ao acessar(dados nulos): {e}") # EM CASO DE DADOS NULOS
+    except Exception as e:
+        print(f"Erro ao salvar dados: {e}") # no CSV
+
+def menu():
+    arquivo_csv = 'dados.csv' # escolhe o nome do arquivo que vai ser salvo os dados
+    while True:
+        print("\nMenu")
+        print("1. Inserir CNPJ")
+        print("2. Sair")
+        opcao = input("Escolha uma opção: ")
+
+        if opcao == '1':
+            cnpj = input("Digite o CNPJ (somente números): ")
+            dados = data(cnpj)
+            if dados:
+                save(cnpj, dados, arquivo_csv) 
+                print("Dados salvos com sucesso!")
+            else:
+                print("Não foi possível obter os dados do CNPJ.")
+        elif opcao == '2':
+            for i in tqdm(range(10), desc="Carregando"):
+                time.sleep(0.05)
+            print("Saindo...")
+            break
+        else:
+            print("Opção inválida. Tente novamente.")
+
+menu()
---
+++ tkk.py
@@ -0,0 +1,102 @@
+import requests
+import csv
+import tkinter as tk
+from tkinter import messagebox
+from tkinter import filedialog
+import os
+
+def data(cnpj):
+    url = f"https://publica.cnpj.ws/cnpj/{cnpj}"
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        dados = response.json()
+        return dados
+    except requests.RequestException as e:
+        print(f"Erro ao obter dados: {e}") #caso de falha ou limites atingidos
+        return None
+
+def type(natureza_juridica): #caso de falha ou limites atingidos
+    if natureza_juridica:
+        descricao = natureza_juridica.lower()
+        if 'municipal' in descricao:
+            return 'Municipal'
+        elif 'estadual' in descricao:
+            return 'Estadual'
+        elif 'federal' in descricao:
+            return 'Federal'
+    return 'Não Especificado'
+
+def save(cnpj, dados, arquivo):
+    try:
+        file_exists = os.path.isfile(arquivo)
+        if file_exists:
+            with open(arquivo, 'r', newline='', encoding='utf-8') as file:
+                first_line = file.readline()
+                if not first_line.strip():  # arquivo estiver vazio
+                    file_exists = False
+        
+        with open(arquivo, 'a', newline='', encoding='utf-8') as file:
+            writer = csv.writer(file)
+            if not file_exists:  # poe cabeçalho se o arquivo estiver vazio
+                writer.writerow([
+                    'CNPJ', 'Razão Social', 'Porte', 
+                    'Natureza Jurídica', 'Natureza Tipo', 'Tipo(Matriz/Filial)', 
+                    'Atividade Principal', 'Município', 'UF'
+                ])
+            
+            est = dados.get('estabelecimento', {})
+            cidade = est.get('cidade', {})
+            estado = est.get('estado', {})
+            
+            writer.writerow([
+                cnpj,   # CNPJ digitado pelo usuário
+                dados.get('razao_social', ''),
+                dados.get('porte', {}).get('descricao', ''),
+                dados.get('natureza_juridica', {}).get('descricao', ''),
+                type(dados.get('natureza_juridica', {}).get('descricao', '')),  # natureza juridica
+                est.get('tipo', ''),  # Tipo (Filial ou Matriz)
+                est.get('atividade_principal', {}).get('descricao', ''),
+                cidade.get('nome', '') if cidade else '',  # verifica se cidade não é None e adc o nome
+                estado.get('sigla', '') if estado else '',  # verifica se estado não é None e adc a UF
+            ])
+    except AttributeError as e:
+        print(f"Erro ao acessar(dados nulos): {e}") # EM CASO DE DADOS NULOS
+    except Exception as e:
+        print(f"Erro ao salvar dados: {e}") # no CSV
+
+def source():
+    cnpj = entry_cnpj.get()
+    dados = data(cnpj)
+    if dados:
+        save(cnpj, dados, arquivo_csv)
+        messagebox.showinfo("Sucesso", "Dados salvos com sucesso!")
+    else:
+        messagebox.showerror("Erro", "Não foi possível obter os dados do CNPJ.")
+
+def file():
+    global arquivo_csv
+    arquivo_csv = filedialog.asksaveasfilename(defaultextension=".csv",
+                                             filetypes=[("CSV files", "*.csv")],
+                                             title="Escolha o arquivo CSV")
+    label_arquivo.config(text=f"Arquivo: {arquivo_csv}")
+
+# INTERFACE TKINTER
+root = tk.Tk()
+root.title("Consulta CNPJ")
+
+file_csv = "DADOS.csv"
+
+tk.Label(root, text="Digite o CNPJ (somente números):").pack(ipadx=80, ipady=20)
+entry_cnpj = tk.Entry(root, borderwidth=2, relief="solid",width=20)
+entry_cnpj.pack(pady=5)
+
+tk.Button(root, text="Obter dados",bg='#00f234',borderwidth=2, relief="solid", command=source).pack(pady=10)
+
+tk.Button(root, text="Selecionar Arquivo CSV existente", command=file).pack(pady=5)
+label_arquivo = tk.Label(root, text=f"Arquivo (autocriação): {file_csv}") #exibir nome de arquivo selecionado
+label_arquivo.pack(pady=5)
+
+tk.Button(root, text="Sair", command=root.quit,bg='RED',borderwidth=2, relief="solid").pack(pady=10) #BOTÃO PARA SAIR
+
+root.mainloop()
